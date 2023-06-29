688a6f1295fc7751b3e1015c1df20cea80814028
muhammadkhadhafi <mhmmdkhadafi0707@gmail.com>
2023-06-29 20:43:31 +0700

    mengubah warna button edit pada mahasiswa dan pegawai

--- resources/views/admin/master-data/pegawai/show.blade.php
+++ resources/views/admin/master-data/pegawai/show.blade.php
@@ -15,7 +15,7 @@
       <div class="card shadow mb-4">
         <div class="card-header d-flex justify-content-between align-items-center">
           <h6 class="m-0 font-weight-bold text-primary text-uppercase">Data Pegawai</h6>
-          <a href="/admin/master-data/pegawai/{{ $pegawai->id }}/edit" class="btn btn-sm btn-warning"><i
+          <a href="/admin/master-data/pegawai/{{ $pegawai->id }}/edit" class="btn btn-sm btn-primary"><i
               class="fas fa-pencil-alt fa-sm"></i> Edit</a>
         </div>
         <div class="card-body">
